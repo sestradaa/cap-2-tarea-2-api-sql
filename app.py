0750b57fb23888513efaa9a060595b8deda3061e
sestradaa <sestradaa2015@hotmail.com>
2024-11-08 22:27:34 -0500

    examen2

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def home():
 @app.route('/post', methods=['GET'])
 def get_all_post():
     conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
+    posts = conn.execute('SELECT * FROM alumnos').fetchall()
     conn.close()
    
     return render_template(template_name_or_list="post/posts.html", posts=posts)
@@ -31,23 +31,26 @@ def get_all_post():
 def get_one_post(post_id):
     if request.method == "GET":
         conn = get_db_connection()
-        post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+        post = conn.execute('SELECT * FROM alumnos WHERE id = ?', (post_id,)).fetchone()
         conn.close()
     return render_template('post/post.html', post=post)
 
 @app.route('/post/create', methods=["GET", "POST"])
 def create_one_post():
     if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        aprobado = request.form['aprobado']
+        nota = request.form['nota']
+        fecha = request.form['fecha']
         mensaje = ""
-        if (title.strip() == ""):
-            return render_template("post/create.html", mensaje = "Ingrese título")
-        elif  (content.strip() == ""):
-            return render_template("post/create.html", mensaje = "Ingrese Contenido")
+        if (nombre.strip() == ""):
+            return render_template("post/create.html", mensaje = "Ingrese nombre")
+        elif  (apellido.strip() == ""):
+            return render_template("post/create.html", mensaje = "Ingrese apellido")
         else:   
             conn = get_db_connection()
-            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+            conn.execute('INSERT INTO alumnos (nombre, apellido,aprobado,nota,fecha) VALUES (?, ?)', (nombre, apellido, aprobado,nota, fecha))
             conn.commit()
             conn.close()
             return redirect(url_for("get_all_post"))
@@ -58,25 +61,28 @@ def create_one_post():
 @app.route('/post/edit/<int:post_id>', methods=['GET', 'POST'])
 def edit_one_post(post_id):
     conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    post = conn.execute('SELECT * FROM alumnos WHERE id = ?', (post_id,)).fetchone()
     #if post: si encontro el registro
     conn.close()
     
     if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        aprobado = request.form['aprobado']
+        nota = request.form['nota']
+        fecha = request.form['fecha']
         mensaje = ""
-        if (title.strip() == ""):
-            context = {'post_error':post_id, 'newTitle' : title, 'newContent' : content, "mensaje": "Ingrese título"}
+        if (nombre.strip() == ""):
+            context = {'post_error':post_id, 'newTitle' : nombre, 'newContent' : apellido, "mensaje": "Ingrese nombre"}
             return render_template(template_name_or_list="post/edit.html", **context)
             #return render_template("post/edit.html", mensaje = "Ingrese título")
-        elif  (content.strip() == ""):
-            context = {'post_error':post_id, 'newTitle' : title, 'newContent' : content, "mensaje": "Ingrese Contenido"}
+        elif  (apellido.strip() == ""):
+            context = {'post_error':post_id, 'newTitle' : nombre, 'newContent' : apellido, "mensaje": "Ingrese apellido"}
             return render_template(template_name_or_list="post/edit.html", **context)
             #return render_template("post/edit.html", **context)
         else: 
             conn = get_db_connection()
-            conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, post_id))
+            conn.execute('UPDATE alumnos SET nombre = ?, apellido = ?, aprobado = ?, nota = ?, fecha = ? WHERE id = ?', (nombre, apellido, aprobado,nota, fecha, post_id))
             conn.commit()
             conn.close()
             return redirect(url_for('get_all_post'))
@@ -89,7 +95,7 @@ def delete_one_post(post_id):
     conn = get_db_connection()
 
     if request.method == "POST":
-        conn.execute('DELETE FROM posts WHERE id = ?', (post_id,))
+        conn.execute('DELETE FROM alumnos WHERE id = ?', (post_id,))
         conn.commit()
         conn.close()
 
