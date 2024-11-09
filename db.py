e04025b3c3b813062939b4f90e483e4add4f6ab8
sestradaa <sestradaa2015@hotmail.com>
2024-11-08 22:31:01 -0500

    examen4

--- db.py
+++ db.py
@@ -2,6 +2,7 @@ import sqlite3
 
 
 def get_db_connection():
-    conn = sqlite3.connect('database.db')
+    #conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect('alumnos.db')
     conn.row_factory = sqlite3.Row
     return conn
